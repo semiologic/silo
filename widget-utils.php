727cb5ca9b6676214104ca8dabc33c16429a9ea9
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-13 17:46:07 +0000

    Fix #104
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/silo/trunk@74 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- widget-utils.php
+++ widget-utils.php
@@ -90,7 +90,11 @@ class widget_utils
 		echo '<ul>';
 		do_action($type . '_widget_config_affected');
 		echo '</ul>';
-	
+		
+		echo '<p>'
+			. 'It will <b>NOT</b> affect anything else. In particular WordPress\'s built-in Pages widget. (Use the Silo Pages widget instead.)'
+			. '</p>';
+		
 		echo '<table style="width: 100%;">';
 		
 		echo '<tr valign="top">' . "\n"
