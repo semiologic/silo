85bb2653726dcad3055b4e577f4f4779398c68a2
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-04-02 14:31:37 +0000

    implement widget cache flush on sidebars_widgets update, and force such a call on every POST action on that page
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/silo/trunk@985 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- silo.php
+++ silo.php
@@ -38,6 +38,9 @@ class silo
 				'update_option_show_on_front',
 				'update_option_page_on_front',
 				'update_option_page_for_posts',
+				'update_option_sidebars_widgets',
+				'update_option_sem5_options',
+				'update_option_sem6_options',
 				'generate_rewrite_rules',
 				) as $hook)
 		{
