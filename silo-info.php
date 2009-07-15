a1851732efbcfab0637fb91fc02a045308792be0
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-15 18:06:25 +0000

    silo rc2
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/silo/trunk@2144 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ silo-info.php
@@ -0,0 +1,4 @@
+<?php
+__('Silo Widgets', 'silo');
+__('Silo web design tools for sites built using static pages.', 'silo');
+?>
--- silo.php
+++ silo.php
@@ -3,7 +3,7 @@
 Plugin Name: Silo Widgets
 Plugin URI: http://www.semiologic.com/software/silo/
 Description: Silo web design tools for sites built using static pages.
-Version: 3.0 RC
+Version: 3.0 RC2
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: silo
