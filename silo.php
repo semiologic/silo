dc8015da4f9570ccce8f7b6608381c9b49698963
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-14 14:46:20 +0000

    cache fixes
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/silo/trunk@3729 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- silo.php
+++ silo.php
@@ -341,15 +341,16 @@ class silo_map extends WP_Widget {
 		if ( !$post || $post->post_type != 'page' || wp_is_post_revision($post_id) )
 			return;
 		
-		if ( wp_cache_get($post_id, 'pre_flush_post') === false )
+		$old = wp_cache_get($post_id, 'pre_flush_post');
+		if ( $old === false )
 			$old = array();
 		
 		$update = false;
 		foreach ( array(
 			'post_title',
 			'post_status',
-			) as $field => $value ) {
-			if ( !isset($o[$field]) ) {
+			) as $field ) {
+			if ( !isset($old[$field]) ) {
 				$old[$field] = $post->$field;
 				$update = true;
 			}
@@ -369,6 +370,7 @@ class silo_map extends WP_Widget {
 				$update = true;
 			}
 		}
+		
 		
 		if ( $update )
 			wp_cache_set($post_id, $old, 'pre_flush_post');
@@ -1021,15 +1023,16 @@ class silo_stub extends WP_Widget {
 		if ( !$post || $post->post_type != 'page' || wp_is_post_revision($post_id) )
 			return;
 		
-		if ( wp_cache_get($post_id, 'pre_flush_post') === false )
+		$old = wp_cache_get($post_id, 'pre_flush_post');
+		if ( $old === false )
 			$old = array();
 		
 		$update = false;
 		foreach ( array(
 			'post_title',
 			'post_status',
-			) as $field => $value ) {
-			if ( !isset($o[$field]) ) {
+			) as $field ) {
+			if ( !isset($old[$field]) ) {
 				$old[$field] = $post->$field;
 				$update = true;
 			}
@@ -1049,6 +1052,7 @@ class silo_stub extends WP_Widget {
 				$update = true;
 			}
 		}
+		
 		
 		if ( $update )
 			wp_cache_set($post_id, $old, 'pre_flush_post');
